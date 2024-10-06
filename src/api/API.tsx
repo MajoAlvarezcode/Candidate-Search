f7f724c6172d2d2e2fe397a97118bf8b33f83869
Majo Alvarez <majo.bootcamp@gmail.com>
2024-10-06 15:51:36 -0600

    added Token

---
+++ src/api/API.tsx
@@ -0,0 +1,44 @@
+const searchGithub = async () => {
+  try {
+    const start = Math.floor(Math.random() * 100000000) + 1;
+    // console.log(import.meta.env);
+    const response = await fetch(
+      `https://api.github.com/users?since=${start}`,
+      {
+        headers: {
+          Authorization: `Bearer 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`,
+        },
+      }
+    );
+    // console.log('Response:', response);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    // console.log('Data:', data);
+    return data;
+  } catch (err) {
+    // console.log('an error occurred', err);
+    return [];
+  }
+};
+
+const searchGithubUser = async (username: string) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`, {
+      headers: {
+        Authorization: `Bearer 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`,
+      },
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error('invalid API response, check the network tab');
+    }
+    return data;
+  } catch (err) {
+    // console.log('an error occurred', err);
+    return {};
+  }
+};
+
+export { searchGithub, searchGithubUser };
