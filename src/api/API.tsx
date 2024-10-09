3eada298e55d20c9151876cb86ff0642663f5d0b
Majo Alvarez <majo.bootcamp@gmail.com>
2024-10-08 21:53:59 -0600

    changed env to make key secret with gitignore

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,12 +1,12 @@
 const searchGithub = async () => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
-    // console.log(import.meta.env);
-    const response = await fetch(
+    console.log(import.meta.env);
+    const response = await fetch( 
       `https://api.github.com/users?since=${start}`,
       {
         headers: {
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
         },
       }
     );
@@ -27,7 +27,7 @@ const searchGithubUser = async (username: string) => {
   try {
     const response = await fetch(`https://api.github.com/users/${username}`, {
       headers: {
-        Authorization: `Bearer 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`,
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
       },
     });
     const data = await response.json();
